5a9d2677c9355d23d52ac4531c5f4be85fcee11e
Sergey Opolko <opolkosergey@gmail.com>
2015-10-21 17:06:51 +0300

    Task1 with tests

---
+++ Task1.Tests/PolynomialTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    internal class PolynomialTests
+    {
+       public IEnumerable<TestCaseData> TestMul
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(0.0, 4, 3), new Polynomial(8, 3), new Polynomial(0, 0, 32, 36, 9)).Returns(true);
+                yield return new TestCaseData(new Polynomial(4), new Polynomial(8, 3), new Polynomial(0, 0, 32, 36, 9)).Returns(false);
+                yield return new TestCaseData(new Polynomial(-5), new Polynomial(8), new Polynomial(0, -40)).Returns(true);
+                yield return new TestCaseData(new Polynomial(1,2), new Polynomial(3,4), new Polynomial(0, 3, 10, 8)).Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestMul))]
+        public bool Mul_Test(Polynomial a, Polynomial b,Polynomial result)
+        {
+            return result.Equals(a * b);
+        }
+
+        public IEnumerable<TestCaseData> TestPlus
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(1, 4, 3), new Polynomial(8, 3), new Polynomial(9, 7, 3)).Returns(true);
+                yield return new TestCaseData(new Polynomial(4), new Polynomial(8, 3), new Polynomial(12,3)).Returns(true);
+                yield return new TestCaseData(new Polynomial(-5), new Polynomial(8), new Polynomial(40)).Returns(false);
+                yield return new TestCaseData(new Polynomial(0, 2), new Polynomial(0,0,3, 4), new Polynomial(0, 2, 3, 4)).Returns(true);
+                yield return new TestCaseData(new Polynomial(-5,-10), new Polynomial(-2), new Polynomial(-7,-10)).Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestPlus))]
+        public bool Plus_Test(Polynomial a, Polynomial b, Polynomial result)
+        {
+            return result.Equals(a + b);
+        }
+
+        public IEnumerable<TestCaseData> TestMinus
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(1, 4, 3), new Polynomial(8, 3), new Polynomial(-7, 1, 3)).Returns(true);
+                yield return new TestCaseData(new Polynomial(4), new Polynomial(8, 3), new Polynomial(-4, 3)).Returns(true);
+                yield return new TestCaseData(new Polynomial(-5), new Polynomial(8), new Polynomial(13)).Returns(false);
+                yield return new TestCaseData(new Polynomial(0, 2), new Polynomial(1, 0, 3, 4), new Polynomial(-1, 2, 3, 4)).Returns(true);
+                yield return new TestCaseData(new Polynomial(-5,-8,-10), new Polynomial(-2,-1), new Polynomial(-3,-7,-10)).Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestMinus))]
+        public bool Minus_Test(Polynomial a, Polynomial b, Polynomial result)
+        {
+            return result.Equals(a - b);
+        }
+    }
+}
--- Task1/Polynomial.cs
+++ Task1/Polynomial.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
     public class Polynomial
     {
+        public delegate double ArithmeticalOperation(double x, double y);
         private readonly double[] _multipliers;
-
+        #region .ctors
         public Polynomial(params double[] multipliers)
         {
+            if (multipliers.Length == 0)
+                throw new ArgumentNullException("The value is null" + nameof(multipliers));
             _multipliers = new double[multipliers.Length];
             _multipliers = multipliers;
         }
+        #endregion
 
+        #region public methods
         public override int GetHashCode()
         {
             var code = _multipliers.Select((m, i) => m * (i + 1)).Sum();
@@ -27,19 +29,110 @@ namespace Task1
             return this.GetHashCode() == obj?.GetHashCode();
         }
 
-        public static bool operator == (Polynomial a, Polynomial b)
+        public static bool operator ==(Polynomial a, Polynomial b)
         {
-            if(a == null || b == null) 
-                throw new NullReferenceException();
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
             return a.Equals(b);
         }
 
-        public static bool operator != (Polynomial a, Polynomial b)
+        public static bool operator !=(Polynomial a, Polynomial b)
         {
-            if (a == null || b == null)
-                throw new NullReferenceException();
-            return a.Equals(b);
+            return !(a == b);
         }
 
+        public static Polynomial operator +(Polynomial a, Polynomial b)
+        {
+            if (a == null)
+                throw new ArgumentNullException("The value is null" + nameof(a));
+            if (b == null)
+                throw new ArgumentNullException("The value is null" + nameof(b));
+            return Calculate(a, b, PlusOperation);
+        }
+
+        public static Polynomial operator -(Polynomial a, Polynomial b)
+        {
+            if (a == null)
+                throw new ArgumentNullException("The value is null" + nameof(a));
+            if (b == null)
+                throw new ArgumentNullException("The value is null" + nameof(b));
+            return Calculate(a, b, MinusOperation);
+        }
+
+        public static Polynomial operator *(Polynomial a, Polynomial b)
+        {
+            if (a == null)
+                throw new ArgumentNullException("The value is null" + nameof(a));
+            if (b == null)
+                throw new ArgumentNullException("The value is null" + nameof(b));
+
+            var multipliers = new double[a._multipliers.Length + b._multipliers.Length];
+
+            for (int i = 0; i < a._multipliers.Length; i++)
+                for (int j = 0; j < b._multipliers.Length; j++)
+                {
+                    multipliers[i + j + 1] += a._multipliers[i]*b._multipliers[j];
+                }
+
+            return new Polynomial(multipliers);
+        }
+        #endregion
+
+        #region private methods
+        private static double MinusOperation(double a, double b)
+        {
+            return a - b;
+        }
+
+        private static double PlusOperation(double a, double b)
+        {
+            return a + b;
+        }
+
+        private static Polynomial Calculate(Polynomial a, Polynomial b, ArithmeticalOperation methodArithmeticalOperation)
+        {
+            var multipliers = a._multipliers.Length > b._multipliers.Length ? new double[a._multipliers.Length] : new double[b._multipliers.Length];
+            int i = 0;
+            double m = 0.0;
+
+            try
+            {
+                do
+                {
+                    m = methodArithmeticalOperation(a._multipliers[i], b._multipliers[i]);
+                    multipliers[i] = m;
+                    ++i;
+                } while (i <= a._multipliers.Length && i <= b._multipliers.Length);
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                if (a._multipliers.Length - 1 < i)
+                {
+                    while (i < b._multipliers.Length)
+                    {
+                        multipliers[i] = b._multipliers[i];
+                        ++i;
+                    }
+                }
+                else
+                {
+                    while (i < a._multipliers.Length)
+                    {
+                        multipliers[i] = a._multipliers[i];
+                        ++i;
+                    }
+                }
+            }
+            return new Polynomial(multipliers);
+        }
+        #endregion
     }
 }
